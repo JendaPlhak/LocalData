7229f5ee0a05ca78062fc1aa2cb2ea53a12b1555
Zdenek Softic <zdenek@softic.cz>
2016-06-19 00:09:28 +0200

    Address validation, wip

---
+++ parser/address_utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# vyheldat kandidaty v textu
+# zkusit match na tabulce adresnich mist (ulic)
+# posledni resort zkusit
+
+
+def validate_address(address, city="Prague", country="CZ"):
+    pass
