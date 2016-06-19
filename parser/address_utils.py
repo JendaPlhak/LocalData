904578e45b0224e4d3122273b2b683a7819d4d8d
Zdenek Softic <zdenek@softic.cz>
2016-06-19 08:36:11 +0200

    Address search + geocoding, wip

--- parser/address_utils.py
+++ parser/address_utils.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 import geocoder
@@ -15,20 +16,25 @@ import redis
 class AddressValidator:
 
     ADDRESS_TABLE_FILE = os.path.join(os.path.dirname(__file__), os.path.pardir, 'datasets/adresni_mista_wgs84_praha.csv')
+
+    # TODO: gogole accuracy: ROOFTOP
     GEOCODERS = [
-        'google',
-        ('mapquest', 'OfSOmTltOxOz7eu5WIvXGFFusOOKERIK'),
-        'osm',
+        ('google', None, None, None),
+        # ('google', None, 'accuracy', ['ROOFTOP']),
+        # ('google', None, 'types', ['street_address']),
+        # ('mapquest', 'OfSOmTltOxOz7eu5WIvXGFFusOOKERIK', None, None),
+        # ('osm', None, None, None)
         # 'here'
     ]
 
     def __init__(self, street, descr_num=None, house_num=None, city='Prague', country='CZ'):
         self.street = street
-        self.descr_num = descr_num
+        self.descr_num = float(descr_num) if descr_num else None
         self.house_num = int(house_num) if house_num else None
         self.city = city
         self.country = country
 
+        self.accuracy = None
         self.source = None
 
         self.r = redis.StrictRedis(host='localhost', port=6379, db=1)
@@ -37,11 +43,11 @@ class AddressValidator:
         line = self.street
 
         if self.descr_num and self.house_num:
-            line = "%s %s/%s" % (line, self.descr_num, self.house_num)
+            line = "%s %s/%s" % (line, int(self.descr_num), int(self.house_num))
         elif self.descr_num:
-            line = "%s %s" % (line, self.descr_num)
+            line = "%s %s" % (line, int(self.descr_num))
         elif self.house_num:
-            line = "%s %s" % (line, self.house_num)
+            line = "%s %s" % (line, int(self.house_num))
 
         return "%s, %s, %s" % (line, self.city, self.country)
 
@@ -56,11 +62,15 @@ class AddressValidator:
     def step_table(self):
         data = self.load_table()
         # query = (lambda df: ((df["Název ulice"] == self.street) & (df["Číslo orientační"] == self.orent_num)))
-        res = data[(
-            (data["Název ulice"] == self.street) & (
+
+        if self.house_num:
+            comparator = (data["Název ulice"] == self.street) & (
                 (data["Číslo domovní"] == self.descr_num) | (data["Číslo orientační"] == float(self.house_num))
             )
-        )]
+        else:
+            comparator = (data["Název ulice"] == self.street) & (data["Číslo domovní"] == self.descr_num)
+
+        res = data[(comparator)]
 
         if res.empty:
             return None
@@ -74,16 +84,24 @@ class AddressValidator:
     def step_geocoding(self):
         res = None
 
-        geocoders = random.shuffle(self.GEOCODERS)
+        geocoders = copy.deepcopy(self.GEOCODERS)
+        random.shuffle(geocoders)
 
         for gc in geocoders:
-            if isinstance(gc, tuple):
-                gc, key = gc
-                res = getattr(geocoder, gc)(self._get_address_line(), key)
-            else:
-                res = getattr(geocoder, gc)(self._get_address_line())
+            gc, key, accuracy, allowed_accuracy = gc
+            args = [gc]
+            kwargs = {}
+            if key:
+                kwargs["key"] = key
+
+            res = getattr(geocoder, gc)(*args, **kwargs)
 
             if res:
+                if self.accuracy and allowed_accuracy:
+                    self.accuracy = getattr(res, accuracy, None) if accuracy else None
+                    if self.accuracy not in allowed_accuracy:
+                        return None
+
                 self.source = gc
                 return res.latlng
 
@@ -112,6 +130,7 @@ class AddressValidator:
 
 
 if __name__ == '__main__':
-    a = AddressValidator("Bělehradská", descr_num=660, house_num=85)
+    # a = AddressValidator("Bělehradská", descr_num="660", house_num="85")
+    a = AddressValidator("Vodičkova", descr_num="18")
     print("Q:", a._get_address_line())
-    print("A:", a.lat_lon(), a.source)
+    print("A:", a.lat_lon(), a.source, a.accuracy)
--- parser/document_parser.py
+++ parser/document_parser.py
@@ -1,4 +1,6 @@
-import re
+# we need unicode matching provided by alternative regex library
+import regex
+re = regex
 
 
 # TODO: hledat pronajem
@@ -9,6 +11,14 @@ NAME_PATTERN = re.compile(r'záměru? prodeje', re.IGNORECASE)
 HOUSE_NUM_PATTERN = re.compile(r'č\.? ?p\. (?P<num>[0-9]+)', re.IGNORECASE)
 PARCEL_NUM_PATTERN = re.compile(r'parc\. ?č\. (?P<num>[0-9]+/?[0-9]*)', re.IGNORECASE)
 
+# TODO: stopwords
+ADDRESS_PATTERN = re.compile(r' (?P<street>'
+                             r'(?:U |Na |Pod |náměstí |nám |nám. )?'
+                             r'(?:\p{Lu}\p{Ll}+)'
+                             r'(?: \p{Ll}{3,})?'
+                             r')'
+                             r' (?P<num>\d+(?:/\d+)?)', re.U)
+
 # PRICE_PATTERN = re.compile(r'(?P<price>\d[\. \d]*),(?:\-|\d{2})')
 # PRICE_PATTERN = re.compile(r'(?P<price>\d[\. \d]*)(?:[,\.]\-|[,\.]\d{2}| Kč)')
 # PRICE_PATTERN = re.compile(r'(?<=(\D|^))(?P<price>\d{1,3}(?:[\. ]\d{3})*)([,\.]\-|[,\.]\d{2}| Kč)')
@@ -16,6 +26,7 @@ PRICE_PATTERN = re.compile(
     r'(\D|^)(?P<price>\d+|(\d{1,3}(?:[\. ]\d{3})*))([,\.]\-|[,\.]\d{2}(?=\s)| Kč)(?P<type>(?:\/|1)(?:(m|rn)2))?')
 
 
+# TODO: search multiple
 def parse_document(data):
     res = {
         "dashboard_id": data["dashboard_id"],
@@ -27,28 +38,32 @@ def parse_document(data):
     doc_text = data['doc_name']
     doc_content = data['doc_text_content']
 
+    # TODO
     name_sale = NAME_PATTERN.search(doc_text)
     if not name_sale:
         return []
+
+    address = ADDRESS_PATTERN.search(doc_content)
+    if not address:
+        return []
+
+    res['address_street'] = address.group("street")
+    res['address_num'] = address.group("num")
 
     parcel_num = HOUSE_NUM_PATTERN.search(doc_content)
     if parcel_num:
         res["estate_id"] = "parcel_number"
         res["number"] = parcel_num.group("num")
-    else:
-        house_num = HOUSE_NUM_PATTERN.search(doc_content)
-        if house_num:
-            res["estate_id"] = "house_number"
-            res["number"] = house_num.group("num")
-        else:
-            return []
+
+    house_num = HOUSE_NUM_PATTERN.search(doc_content)
+    if house_num:
+        res["estate_id"] = "house_number"
+        res["number"] = house_num.group("num")
 
     price = PRICE_PATTERN.search(doc_content)
     if price:
         res["price"] = price.group("price")
         res["price_type"] = "per_meter" if price.group("type") else "total"
-    else:
-        return []
 
     return [res]
 
