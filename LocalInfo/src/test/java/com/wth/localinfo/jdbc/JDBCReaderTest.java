5ae8f35e4c94bcdb16f4b03027d397f988c580f9
zruzicka <zruzicka@email.cz>
2016-06-19 02:31:45 +0200

    (LocalInfo) * -A JDBCReader, utils, table_header.

---
+++ LocalInfo/src/test/java/com/wth/localinfo/jdbc/JDBCReaderTest.java
@@ -0,0 +1,26 @@
+package com.wth.localinfo.jdbc;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.wth.localinfo.TestConsts;
+import com.wth.localinfo.TestUtils;
+import com.wth.localinfo.csv.LocalCSVReader;
+
+public class JDBCReaderTest {
+
+    private final JDBCReader reader;
+
+    public JDBCReaderTest() throws IOException {
+        String[] header = new LocalCSVReader().loadHeader(TestConsts.CSV_TABLE_HEADER_FILE_NAME);
+        reader = new JDBCReader(header);
+    }
+
+    @Test
+    public void testDataPrint() throws Exception {
+        int defaultLimit = 3;
+        TestUtils.consolePrint(reader.load(defaultLimit));
+    }
+
+}
